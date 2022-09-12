37dce9a7dcda836b58b1f3ae541758a4cf16b193
Rodrigo Antonio <rodrigo.vidal@virginiogomez.cl>
2022-09-12 12:58:21 -0300

    Se agrega lectura de archivo con express

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const fs = require('fs')
+
+const app = express()
+const PORT = 8080
+const server = app.listen(PORT,() => {
+    console.log(`Servidor http escuchando en puerto ${server.address().port}`)
+})
+server.on('error',error => console.log(`Error en el servidor ${error}`))
+
+app.get('/productos',(req,res) => {
+    let data = fs.readFileSync('./data/productos.json','utf-8')
+    data = JSON.parse(data)
+    res.json(data)
+})
+
+app.get('/productoRandom',(req,res) => {
+    let data = fs.readFileSync('./data/productos.json','utf-8')
+    data = JSON.parse(data)
+    const total = (data.length - 1)
+    const indice = Math.floor(Math.random()*(total-0+1)+0)
+    res.json(data[indice])
+})
